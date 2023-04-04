83bd972c8cc6ed1524168e984e27832e2596e4ff
komaeka <komaeka@163.com>
2023-04-04 12:06:47 +0800

    add sepolia testnet

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,9 @@ import { defineStore } from 'pinia'
 
 export const useContract = defineStore('contractAttributes', () => {
     const contractAttributes = reactive({
-        // 测试合约
-        // contractAddress: '0xFCe59FfF225D8F2da36230b57473fFec14581bea',
-        // 正式合约
-        contractAddress: '0xBd9AA5E6eC442CA8dE74BF8D829e95c3b38b96c0',
-        contractAbi: '[{"inputs":[],"outputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"name":"_post_title","type":"string"},{"name":"_post_content","type":"string"},{"name":"_publish_time","type":"uint256"},{"name":"_picture_hash","type":"string"}],"name":"publish_post","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_comment","type":"string"},{"name":"_post_position","type":"uint8"}],"name":"publish_comment","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"get_posts_titles","outputs":[{"components":[{"name":"post_title","type":"string"},{"name":"post_position","type":"uint8"},{"name":"publish_time","type":"uint256"}],"name":"","type":"tuple[100]"}],"stateMutability":"view","type":"function"},{"inputs":[{"name":"_post_position","type":"uint8"}],"name":"get_post_object","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string[]"}],"stateMutability":"view","type":"function"}]'
+        // Sepolia测试链
+        contractAddress: '0x5686971Af13b01196db61D2614d1B944e8D6EBe9',
+        contractAbi: '[{"inputs":[{"name":"_post_title","type":"string"},{"name":"_post_content","type":"string"},{"name":"_publish_time","type":"uint256"},{"name":"_picture_hash","type":"string"}],"name":"publish_post","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_comment","type":"string"},{"name":"_post_position","type":"uint8"}],"name":"publish_comment","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"get_posts_titles","outputs":[{"components":[{"name":"post_title","type":"string"},{"name":"post_position","type":"uint8"},{"name":"publish_time","type":"uint256"}],"name":"","type":"tuple[100]"}],"stateMutability":"view","type":"function"},{"inputs":[{"name":"_post_position","type":"uint8"}],"name":"get_post_object","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string[]"}],"stateMutability":"view","type":"function"}]'
     })
     return { contractAttributes }
 })
