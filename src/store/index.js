6115fd4485d20f3ae4360da30cd4f9a46c004c50
komaeka <komaeka@163.com>
2023-04-04 14:02:24 +0800

    revise contract address

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia'
 export const useContract = defineStore('contractAttributes', () => {
     const contractAttributes = reactive({
         // Sepolia测试链
-        contractAddress: '0x5686971Af13b01196db61D2614d1B944e8D6EBe9',
+        contractAddress: '0x0Cc8fdF80A217CD266699707D4a3Ef4A8ea7C097',
         contractAbi: '[{"inputs":[{"name":"_post_title","type":"string"},{"name":"_post_content","type":"string"},{"name":"_publish_time","type":"uint256"},{"name":"_picture_hash","type":"string"}],"name":"publish_post","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_comment","type":"string"},{"name":"_post_position","type":"uint8"}],"name":"publish_comment","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"get_posts_titles","outputs":[{"components":[{"name":"post_title","type":"string"},{"name":"post_position","type":"uint8"},{"name":"publish_time","type":"uint256"}],"name":"","type":"tuple[100]"}],"stateMutability":"view","type":"function"},{"inputs":[{"name":"_post_position","type":"uint8"}],"name":"get_post_object","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string[]"}],"stateMutability":"view","type":"function"}]'
     })
     return { contractAttributes }
